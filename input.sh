17405a68449e4215e1da7646a4775d4670ba5a39
Andrea Bocci <andrea.bocci@cern.ch>
2018-12-09 17:57:07 +0100

    Update releases and relvals to 10.4.0-pre3

--- input.sh
+++ input.sh
@@ -4,7 +4,7 @@ NUMEVENTS=100
 
 # Datasets used as input for the matrix-like tests
 SAMPLES="TTBAR ZMUMU"
-TTBAR="/RelValTTbar_13/CMSSW_10_4_0_pre2-PU25ns_103X_upgrade2018_realistic_v8-v1/GEN-SIM-DIGI-RAW"
+TTBAR="/RelValTTbar_13/CMSSW_10_4_0_pre3-PU25ns_103X_upgrade2018_realistic_v8-v1/GEN-SIM-DIGI-RAW"
 TTBAR_NUMEVENTS=100
-ZMUMU="/RelValZMM_13/CMSSW_10_4_0_pre2-103X_upgrade2018_realistic_v8-v1/GEN-SIM-DIGI-RAW"
+ZMUMU="/RelValZMM_13/CMSSW_10_4_0_pre3-103X_upgrade2018_realistic_v8-v1/GEN-SIM-DIGI-RAW"
 ZMUMU_NUMEVENTS=200
