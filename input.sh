96e13e4a750396f67ac0eef65b50c0a375a1f5e0
Andrea Bocci <andrea.bocci@cern.ch>
2019-05-21 08:02:08 +0200

    Update release, samples and global tag to CMSSW 10.6.0

--- input.sh
+++ input.sh
@@ -1,19 +1,19 @@
 # Default global tag and number of events
-GLOBALTAG="105X_upgrade2018_realistic_v2"
+GLOBALTAG="106X_upgrade2018_realistic_v4"
 NUMEVENTS=100
 
 # Datasets used as input for the matrix-like tests
 SAMPLES="TTBAR ZMUMU" # TTBARDESIGN
 
-# TTbar sample. with "realistic" conditions and 50 pileup
-TTBAR="/RelValTTbar_13/CMSSW_10_5_0_pre2-PU25ns_105X_upgrade2018_realistic_v2-v1/GEN-SIM-DIGI-RAW"
+# TTbar sample, with "realistic" conditions and 50 pileup
+TTBAR="/RelValTTbar_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_realistic_v4-v1/GEN-SIM-DIGI-RAW"
 TTBAR_NUMEVENTS=100
 
 # Z -> mumu sample, with "realistic" conditions and no pileup
-ZMUMU="/RelValZMM_13/CMSSW_10_5_0_pre2-105X_upgrade2018_realistic_v2-v1/GEN-SIM-DIGI-RAW"
+ZMUMU="/RelValZMM_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_realistic_v4-v1/GEN-SIM-DIGI-RAW"
 ZMUMU_NUMEVENTS=200
 
-# TTbar sample. with "design" conditions and 50 pileup
-TTBARDESIGN="/RelValTTbar_13/CMSSW_10_5_0_pre2-PU25ns_105X_upgrade2018_design_v2-v1/GEN-SIM-DIGI-RAW"
+# TTbar sample, with "design" conditions and 50 pileup
+TTBARDESIGN="/RelValTTbar_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_design_v3-v1/GEN-SIM-DIGI-RAW"
 TTBARDESIGN_NUMEVENTS=100
-TTBARDESIGN_GLOBALTAG="105X_upgrade2018_design_v2"
+TTBARDESIGN_GLOBALTAG="106X_upgrade2018_design_v3"
