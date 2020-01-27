aec129b00d034fa9ae0d776f7e2f32e0ba5bfb8f
Andrea Bocci <andrea.bocci@cern.ch>
2020-01-27 16:20:23 +0100

    Update relvals to CMSSW 11.0.0-pre12

--- input.sh
+++ input.sh
@@ -1,5 +1,5 @@
 # Default global tag and number of events
-GLOBALTAG="106X_upgrade2018_realistic_v4"
+GLOBALTAG="110X_upgrade2018_realistic_v7"
 NUMEVENTS=100
 
 # Datasets used as input for the matrix-like tests
@@ -9,17 +9,17 @@ SAMPLES="TTBAR ZMUMU TTBARDESIGN"
 DATA_SAMPLES="L1ACCEPT"
 
 # TTbar sample, with "realistic" conditions and 50 pileup
-TTBAR="/RelValTTbar_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_realistic_v4-v1/GEN-SIM-DIGI-RAW"
+TTBAR="/RelValTTbar_13/CMSSW_11_0_0_pre12-PU25ns_110X_upgrade2018_realistic_v7_forTrk-v1/GEN-SIM-DIGI-RAW"
 TTBAR_NUMEVENTS=100
 
 # Z -> mumu sample, with "realistic" conditions and no pileup
-ZMUMU="/RelValZMM_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_realistic_v4-v1/GEN-SIM-DIGI-RAW"
+ZMUMU="/RelValZMM_13/CMSSW_11_0_0_pre12-110X_upgrade2018_realistic_v7_forTrk-v1/GEN-SIM-DIGI-RAW"
 ZMUMU_NUMEVENTS=200
 
 # TTbar sample, with "design" conditions and 50 pileup
-TTBARDESIGN="/RelValTTbar_13/CMSSW_10_6_0-PU25ns_106X_upgrade2018_design_v3-v1/GEN-SIM-DIGI-RAW"
+TTBARDESIGN="/RelValTTbar_13/CMSSW_11_0_0_pre12-PU25ns_110X_upgrade2018_design_v3_forTrk-v1/GEN-SIM-DIGI-RAW"
 TTBARDESIGN_NUMEVENTS=100
-TTBARDESIGN_GLOBALTAG="106X_upgrade2018_design_v3"
+TTBARDESIGN_GLOBALTAG="110X_upgrade2018_design_v3"
 
 # Level-1 Trigger selected events, from Run2018D data taking era, pileup 50
 L1ACCEPT="/EphemeralHLTPhysics1/Run2018D-v1/RAW run=323775 lumi=53"
