669166cec58cef4d92cbb9fd9b35db5c6b30854b
Andrea Bocci <andrea.bocci@cern.ch>
2020-08-11 11:59:47 +0200

    Update relvals and global tag to CMSSW_11_2_0_pre3

--- input.sh
+++ input.sh
@@ -1,5 +1,5 @@
 # Default global tag and number of events
-GLOBALTAG="111X_mcRun3_2021_realistic_v4"
+GLOBALTAG="112X_mcRun3_2021_realistic_v5"
 NUMEVENTS=200
 
 # Datasets used as input for the matrix-like tests
@@ -9,15 +9,15 @@ SAMPLES="TTBAR ZMUMU ZEE"
 DATA_SAMPLES="L1ACCEPT"
 
 # TTbar sample, with 2021 "realistic" conditions and pileup
-TTBAR="/RelValTTbar_14TeV/CMSSW_11_1_0_pre8-PU_111X_mcRun3_2021_realistic_v4-v1/GEN-SIM-DIGI-RAW"
+TTBAR="/RelValTTbar_14TeV/CMSSW_11_2_0_pre3-PU_112X_mcRun3_2021_realistic_v5-v1/GEN-SIM-DIGI-RAW"
 TTBAR_NUMEVENTS=100
 
 # Z -> mumu sample, with 2021 "realistic" conditions, no pileup
-ZMUMU="/RelValZMM_14/CMSSW_11_1_0_pre8-111X_mcRun3_2021_realistic_v4-v1/GEN-SIM-DIGI-RAW"
+ZMUMU="/RelValZMM_14/CMSSW_11_2_0_pre3-112X_mcRun3_2021_realistic_v5-v1/GEN-SIM-DIGI-RAW"
 ZMUMU_NUMEVENTS=200
 
 # Z -> ee sample, with 2021 "realistic" conditions, no pileup
-ZEE="/RelValZEE_14/CMSSW_11_1_0_pre8-111X_mcRun3_2021_realistic_v4-v1/GEN-SIM-DIGI-RAW"
+ZEE="/RelValZEE_14/CMSSW_11_2_0_pre3-112X_mcRun3_2021_realistic_v5-v1/GEN-SIM-DIGI-RAW"
 ZEE_NUMEVENTS=200
 
 # Level-1 Trigger selected events, from Run2018D data taking era, pileup 50
